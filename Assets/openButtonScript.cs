03eb622844f1a8245c7b434868eededefa9cbaeb
awalrajsingh <67677406+awalrajsingh@users.noreply.github.com>
2020-12-21 23:47:44 +0530

    ButtonOpener moving to bases

--- Assets/openButtonScript.cs
+++ Assets/openButtonScript.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class openButtonScript : MonoBehaviour
 {
     public Animator buttonOpen;
+    public GameObject buttonOpener;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            buttonOpener.transform.position = new Vector3(this.transform.position.x, buttonOpener.transform.position.y, this.transform.position.z);
             Debug.Log("Car Entered");
             buttonOpen.SetTrigger("EnterButton");
+            
         } 
     
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("CarExit");
+            buttonOpen.SetTrigger("ExitButton");
+            
+        }
     }
 }

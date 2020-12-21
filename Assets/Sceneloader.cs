09a4b3167428ed49ccf467b3e5f46f5de692befa
midopooler <midhapulkit28@gmail.com>
2020-12-22 03:13:53 +0530

    Added splashscene so as to navigate between both the builds in debug

---
+++ Assets/Sceneloader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneloader : MonoBehaviour
+{
+
+  
+    public void LoadAwal()
+    {
+        SceneManager.LoadScene("PortfolioScene");
+    }
+    public void LoadPulkit()
+    {
+        SceneManager.LoadScene("PulkitPortfolio");
+    }
+    
+}

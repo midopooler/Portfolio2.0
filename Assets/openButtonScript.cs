f6f69a5384a332fd3424b43e81b05e1d7c77235f
awalrajsingh <67677406+awalrajsingh@users.noreply.github.com>
2020-12-21 21:09:03 +0530

    button opener anim

---
+++ Assets/openButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openButtonScript : MonoBehaviour
+{
+    public Animator buttonOpen;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Car Entered");
+            buttonOpen.SetTrigger("EnterButton");
+        } 
+    
+    }
+}

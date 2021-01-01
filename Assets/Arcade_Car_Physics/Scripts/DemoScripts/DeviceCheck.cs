3e32da11eaf7ae8d702a24baecf1134124604ba7
awalrajsingh <67677406+awalrajsingh@users.noreply.github.com>
2021-01-02 00:41:36 +0530

    device check

---
+++ Assets/Arcade_Car_Physics/Scripts/DemoScripts/DeviceCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeviceCheck : MonoBehaviour
+{
+    public TextMeshProUGUI devicename;
+    void Start()
+    {
+        if(SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            devicename.text = "moglail phone";
+        } 
+        else
+        {
+            devicename.text = "kamputer";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

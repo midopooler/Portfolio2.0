ef8f592211abe3ecdaa2e787e0f5b40cdad7ce4c
awalrajsingh <67677406+awalrajsingh@users.noreply.github.com>
2021-01-10 23:28:00 +0530

    Streaming assets folder with gif video

---
+++ Assets/videoEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class videoEditor : MonoBehaviour
+{
+    public VideoPlayer video;
+    void Start()
+    {
+       video = this.gameObject.GetComponent<VideoPlayer>();
+        video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "MY INTERESTS (2).mp4");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

7e9191a46cb7df8b18228c2ebd7d909facd81b50
dracielisawal <awal.singh04@gmail.com>
2021-01-23 01:50:54 +0530

    audio on brick collision (still playing same random on all bricks)

---
+++ Assets/audioplayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class audioplayer : MonoBehaviour
+{
+    public AudioClip[] audios;
+    void Start()
+    {
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+
+  void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hit");
+        // if (collision.gameObject.tag == "Player"|| collision.gameObject.tag == "road" || collision.gameObject.tag == "brick")
+        //{ 
+        Random rand = new Random();
+
+        this.gameObject.GetComponent<AudioSource>().clip = audios[Random.Range(0, audios.Length)];
+            this.gameObject.GetComponent<AudioSource>().Play();
+            
+        //}
+    }
+}
